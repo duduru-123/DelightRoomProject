3b6ee4a5f63176c5019842ce26314a29da53f106
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-12 11:35:03 +0900

    Add UserDetail data model

---
+++ app/src/main/java/com/delightroom/android/gitproject/datasource/remote/model/UserDetail.kt
@@ -0,0 +1,80 @@
+package com.delightroom.android.gitproject.datasource.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class UserDetail( 
+    val login: String,
+    val id: Long,
+
+    @SerializedName("node_id")
+    val nodeID: String,
+
+    @SerializedName("avatar_url")
+    val avatarURL: String,
+
+    @SerializedName("gravatar_id")
+    val gravatarID: String,
+
+    val url: String,
+
+    @SerializedName("html_url")
+    val htmlURL: String,
+
+    @SerializedName("followers_url")
+    val followersURL: String,
+
+    @SerializedName("following_url")
+    val followingURL: String,
+
+    @SerializedName("gists_url")
+    val gistsURL: String,
+
+    @SerializedName("starred_url")
+    val starredURL: String,
+
+    @SerializedName("subscriptions_url")
+    val subscriptionsURL: String,
+
+    @SerializedName("organizations_url")
+    val organizationsURL: String,
+
+    @SerializedName("repos_url")
+    val reposURL: String,
+
+    @SerializedName("events_url")
+    val eventsURL: String,
+
+    @SerializedName("received_events_url")
+    val receivedEventsURL: String,
+
+    val type: String,
+
+    @SerializedName("site_admin")
+    val siteAdmin: Boolean,
+
+    val name: String,
+    val company: String,
+    val blog: String,
+    val location: String,
+    val email: Any? = null,
+    val hireable: Any? = null,
+    val bio: Any? = null,
+
+    @SerializedName("twitter_username")
+    val twitterUsername: Any? = null,
+
+    @SerializedName("public_repos")
+    val publicRepos: Long,
+
+    @SerializedName("public_gists")
+    val publicGists: Long,
+
+    val followers: Long,
+    val following: Long,
+
+    @SerializedName("created_at")
+    val createdAt: String,
+
+    @SerializedName("updated_at")
+    val updatedAt: String
+)
