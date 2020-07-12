53523e5e21e9f4119100740436ac6a348a5c5a00
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-12 18:06:16 +0900

    Fix to use pageSize in params

--- app/src/main/java/com/delightroom/android/gitproject/datasource/paging/UsersDataSource.kt
+++ app/src/main/java/com/delightroom/android/gitproject/datasource/paging/UsersDataSource.kt
@@ -6,14 +6,14 @@ import com.delightroom.android.gitproject.datasource.vo.UserVO
 import com.delightroom.android.gitproject.manager.RetrofitManager
 import com.delightroom.android.gitproject.utility.convertToUserVO
 import com.delightroom.android.gitproject.utility.logE
+import com.delightroom.android.gitproject.utility.logI
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class UsersDataSource(
     retrofitManager: RetrofitManager,
-    private val viewModelScope: CoroutineScope,
-    private val pageSize: Int = 30
+    private val viewModelScope: CoroutineScope
 ) : PageKeyedDataSource<Int, UserVO>() {
 
     private val apiInterFace = retrofitManager.createApi(UserService::class.java)
@@ -25,10 +25,12 @@ class UsersDataSource(
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val currentPage = 0
+                val pageSize = params.requestedLoadSize
                 val result = apiInterFace.getUsers(page = currentPage, pageSize = pageSize)
                 val listOfUserVO = result.map { it.convertToUserVO() }.toList()
                 val nextPage = currentPage + 1
 
+                logI("loadInitial size: ${listOfUserVO.size}")
                 callback.onResult(listOfUserVO, null, nextPage)
 
             } catch (e: Exception) {
@@ -45,9 +47,12 @@ class UsersDataSource(
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val currentPage = params.key
+                val pageSize = params.requestedLoadSize
                 val result = apiInterFace.getUsers(page = currentPage, pageSize = pageSize)
                 val listOfUserVO = result.map { it.convertToUserVO() }.toList()
                 val nextPage = currentPage + 1
+
+                logI("loadAfter size: ${listOfUserVO.size}")
 
                 callback.onResult(listOfUserVO, nextPage)
             } catch (e: Exception) {
