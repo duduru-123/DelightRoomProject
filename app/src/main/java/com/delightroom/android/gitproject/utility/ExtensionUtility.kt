e2f10f03eb3b5b30f0b7d02bce411d3dcc840701
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-12 17:19:21 +0900

    Add User.convertToUserVO function for converting User to UserVO

--- app/src/main/java/com/delightroom/android/gitproject/utility/ExtensionUtility.kt
+++ app/src/main/java/com/delightroom/android/gitproject/utility/ExtensionUtility.kt
@@ -2,23 +2,43 @@ package com.delightroom.android.gitproject.utility
 
 import android.util.Log
 import android.view.View
+import com.delightroom.android.gitproject.datasource.remote.model.User
+import com.delightroom.android.gitproject.datasource.vo.UserVO
 
 
+/**
+ * object converting
+ */
+fun User.convertToUserVO(): UserVO {
+    return UserVO(
+        id = this.login ?: ""
+    )
+}
 
+
+/**
+ * visibility
+ */
 fun View.show() {
     this.visibility = View.VISIBLE
 }
+
 fun View.hide() {
     this.visibility = View.INVISIBLE
 }
+
 fun View.gone() {
     this.visibility = View.GONE
 }
 
 
+/**
+ * log
+ */
 fun Any.logI(message: String?, distribution: String = "@@@@@@@@") {
     Log.i(this::class.java.simpleName, "$distribution $message")
 }
+
 fun Any.logE(message: String?, distribution: String = "@@@@@@@@") {
     Log.e(this::class.java.simpleName, "$distribution $message")
 }
