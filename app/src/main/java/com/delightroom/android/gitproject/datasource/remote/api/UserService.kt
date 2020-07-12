2e2a2b9c4024e3821d1cf222b043243175e6ec76
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-12 11:48:39 +0900

    Add getUser api to UserService, UserRepository

--- app/src/main/java/com/delightroom/android/gitproject/datasource/remote/api/UserService.kt
+++ app/src/main/java/com/delightroom/android/gitproject/datasource/remote/api/UserService.kt
@@ -1,11 +1,27 @@
 package com.delightroom.android.gitproject.datasource.remote.api
 
 import com.delightroom.android.gitproject.datasource.remote.model.User
+import com.delightroom.android.gitproject.datasource.remote.model.UserDetail
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface UserService {
 
+
+    /**
+     * request all users
+     */
     @GET("/users")
-    suspend fun getUsers() : List<User>
+    suspend fun getUsers(): List<User>
+
+
+    /**
+     * request user by userId
+     */
+    @GET("/users/{userId}")
+    suspend fun getUser(
+        @Path("userId")
+        userId: String
+    ): UserDetail
 }
--- app/src/main/java/com/delightroom/android/gitproject/repository/UserRepository.kt
+++ app/src/main/java/com/delightroom/android/gitproject/repository/UserRepository.kt
@@ -2,6 +2,7 @@ package com.delightroom.android.gitproject.repository
 
 import com.delightroom.android.gitproject.datasource.remote.api.UserService
 import com.delightroom.android.gitproject.datasource.remote.model.User
+import com.delightroom.android.gitproject.datasource.remote.model.UserDetail
 import com.delightroom.android.gitproject.manager.RetrofitManager
 
 class UserRepository(
@@ -10,9 +11,16 @@ class UserRepository(
 
 
     /**
-     * request users
+     * request all users
      */
     suspend fun requestUsers(): List<User> {
         return retrofitManager.createApi(UserService::class.java).getUsers()
+    }
+
+    /**
+     * request user by userId
+     */
+    suspend fun requestUser(userId: String): UserDetail {
+        return retrofitManager.createApi(UserService::class.java).getUser(userId)
     }
 }
