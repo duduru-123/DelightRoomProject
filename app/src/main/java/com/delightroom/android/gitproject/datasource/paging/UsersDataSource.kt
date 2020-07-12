2eb7fdbc7c0a3fc6a4ad4a83ea9ecefed2963d12
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-12 17:20:18 +0900

    Add UsersDataSource, UserVO for paging

---
+++ app/src/main/java/com/delightroom/android/gitproject/datasource/paging/UsersDataSource.kt
@@ -0,0 +1,72 @@
+package com.delightroom.android.gitproject.datasource.paging
+
+import androidx.paging.PageKeyedDataSource
+import com.delightroom.android.gitproject.datasource.remote.api.UserService
+import com.delightroom.android.gitproject.datasource.vo.UserVO
+import com.delightroom.android.gitproject.manager.RetrofitManager
+import com.delightroom.android.gitproject.utility.convertToUserVO
+import com.delightroom.android.gitproject.utility.logE
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UsersDataSource(
+    retrofitManager: RetrofitManager,
+    private val viewModelScope: CoroutineScope,
+    private val pageSize: Int = 30
+) : PageKeyedDataSource<Int, UserVO>() {
+
+    private val apiInterFace = retrofitManager.createApi(UserService::class.java)
+
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, UserVO>
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val currentPage = 0
+                val result = apiInterFace.getUsers(page = currentPage, pageSize = pageSize)
+                val listOfUserVO = result.map { it.convertToUserVO() }.toList()
+                val nextPage = currentPage + 1
+
+                callback.onResult(listOfUserVO, null, nextPage)
+
+            } catch (e: Exception) {
+
+                logE(e.message)
+            }
+        }
+    }
+
+    override fun loadAfter(
+        params: LoadParams<Int>,
+        callback: LoadCallback<Int, UserVO>
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val currentPage = params.key
+                val result = apiInterFace.getUsers(page = currentPage, pageSize = pageSize)
+                val listOfUserVO = result.map { it.convertToUserVO() }.toList()
+                val nextPage = currentPage + 1
+
+                callback.onResult(listOfUserVO, nextPage)
+            } catch (e: Exception) {
+                logE(e.message)
+            }
+        }
+    }
+
+    override fun loadBefore(
+        params: LoadParams<Int>,
+        callback: LoadCallback<Int, UserVO>
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+            } catch (e: Exception) {
+                logE(e.message)
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/delightroom/android/gitproject/datasource/vo/UserVO.kt
@@ -0,0 +1,5 @@
+package com.delightroom.android.gitproject.datasource.vo
+
+data class UserVO (
+    var id: String
+)
