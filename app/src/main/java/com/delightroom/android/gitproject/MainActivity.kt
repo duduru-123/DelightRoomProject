99e8b3b2630a047d7371a93db46805e73e76e6a0
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-12 15:08:23 +0900

    Update bottomNavMenu for connecting to nav_graph.xml, fragments by same id

--- app/src/main/java/com/delightroom/android/gitproject/MainActivity.kt
+++ app/src/main/java/com/delightroom/android/gitproject/MainActivity.kt
@@ -2,11 +2,24 @@ package com.delightroom.android.gitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.delightroom.android.gitproject.manager.RetrofitManager
+import com.delightroom.android.gitproject.repository.UserRepository
+import com.delightroom.android.gitproject.utility.logE
+import com.delightroom.android.gitproject.utility.logI
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(bottomNavMenu, findNavController(R.id.navHost))
     }
 }
