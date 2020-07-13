7c57ed94a949b55735b4452c6bb21fa10c46843b
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-14 01:15:30 +0900

    Add px, dp high order function

--- app/src/main/java/com/delightroom/android/gitproject/utility/Utility.kt
+++ app/src/main/java/com/delightroom/android/gitproject/utility/Utility.kt
@@ -1,6 +1,7 @@
 package com.delightroom.android.gitproject.utility
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import org.joda.time.DateTime
 import java.text.SimpleDateFormat
 import java.util.*
@@ -33,3 +34,23 @@ fun getDateYYYYMMdd(time: Long): String {
     val format = "YYYY MM dd"
     return getDateFormat(time, format)
 }
+
+/**
+ * High-order function
+ */
+
+/**
+ * dp to px
+ * @return px calculated from dp
+ */
+val Int.px get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+val Float.px get() = (this * Resources.getSystem().displayMetrics.density)
+
+
+/**
+ * px to dp
+ * @return dp calculated from px
+ */
+val Int.dp get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+val Float.dp get() = (this / Resources.getSystem().displayMetrics.density)
+
