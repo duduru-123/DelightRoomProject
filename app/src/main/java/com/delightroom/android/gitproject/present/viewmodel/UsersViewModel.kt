d55f94ce7af579e2956fb0f21089af557ebdc20a
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-14 01:35:43 +0900

    Add RepositoryDataSource and apply to UsersViewModel

--- app/src/main/java/com/delightroom/android/gitproject/present/viewmodel/UsersViewModel.kt
+++ app/src/main/java/com/delightroom/android/gitproject/present/viewmodel/UsersViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.DataSource
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
+import com.delightroom.android.gitproject.datasource.paging.RepositoryDataSource
 import com.delightroom.android.gitproject.datasource.paging.UsersDataSource
+import com.delightroom.android.gitproject.datasource.vo.ReposVO
 import com.delightroom.android.gitproject.datasource.vo.UserVO
 import com.delightroom.android.gitproject.manager.RetrofitManager
 import com.delightroom.android.gitproject.repository.UserRepository
@@ -18,10 +20,26 @@ class UsersViewModel(
 
     // list of userVO
     val listOfUserVO = createListOfUserVOLiveData()
+    val listOfRepositoryReposVO = createListOfReposVOLiveData()
 
 
     /**
-     * create live data
+     * refresh list of userVO
+     */
+    fun refreshListOfUserVO() {
+        listOfUserVO.value?.dataSource?.invalidate()
+    }
+
+    /**
+     * refresh list of ReposVO
+     */
+    fun refreshListOfReposVO() {
+        listOfRepositoryReposVO.value?.dataSource?.invalidate()
+    }
+
+
+    /**
+     * create live data from Users
      */
     private fun createListOfUserVOLiveData(): LiveData<PagedList<UserVO>> {
         val pageSize = 20
@@ -40,9 +58,20 @@ class UsersViewModel(
 
 
     /**
-     * refresh list of userVO
+     * create list of reposVO from Repository
      */
-    fun refreshListOfUserVO() {
-        listOfUserVO.value?.dataSource?.invalidate()
+    private fun createListOfReposVOLiveData(): LiveData<PagedList<ReposVO>> {
+        val pageSize = 20
+        val config = PagedList.Config.Builder()
+            .setInitialLoadSizeHint(pageSize + 10)
+            .setPageSize(pageSize)
+            .setPrefetchDistance(10)
+            .build()
+
+        return LivePagedListBuilder(object : DataSource.Factory<Int, ReposVO>() {
+            override fun create(): DataSource<Int, ReposVO> {
+                return RepositoryDataSource(retrofitManager, viewModelScope)
+            }
+        }, config).build()
     }
 }
