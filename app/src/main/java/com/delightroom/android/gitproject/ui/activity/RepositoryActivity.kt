aefcba9aa2143e2f5766f40ba693697f7a5f66ac
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-14 02:52:44 +0900

    Add RepositoryActivity

---
+++ app/src/main/java/com/delightroom/android/gitproject/ui/activity/RepositoryActivity.kt
@@ -0,0 +1,13 @@
+package com.delightroom.android.gitproject.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.delightroom.android.gitproject.R
+
+class RepositoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository)
+    }
+}
