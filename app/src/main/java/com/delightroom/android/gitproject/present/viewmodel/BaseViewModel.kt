114767c48cb939eed09c6c3a5826adfd081bee6f
DESKTOP-8NU2HQ1\LEE <shfwk32@gmail.com>
2020-07-15 14:03:55 +0900

    Add BaseViewModel for handling error of viewModels

---
+++ app/src/main/java/com/delightroom/android/gitproject/present/viewmodel/BaseViewModel.kt
@@ -0,0 +1,30 @@
+package com.delightroom.android.gitproject.present.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.delightroom.android.gitproject.R
+import com.delightroom.android.gitproject.utility.logE
+import kotlinx.coroutines.*
+
+open class BaseViewModel(private val context: Context) : ViewModel() {
+
+    val job = CoroutineScope(Dispatchers.IO + getExceptionHandler())
+    val toast = MutableLiveData<String>()
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+
+    /**
+     * exception handler
+     */
+    private fun getExceptionHandler() = CoroutineExceptionHandler { _, exception ->
+        logE(exception.message)
+
+        val message = context.getString(R.string.no_data)
+        toast.postValue(message)
+    }
+}
